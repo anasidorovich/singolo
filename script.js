f90af122597ecb80cde575b095e4fe3ffd295017
Anastasiya Sidarovich <anastasiya.sidarovich@digitech7.by>
2020-03-19 13:29:29 +0300

    feat: update slider and portfolio sections

--- script.js
+++ script.js
@@ -3,14 +3,6 @@
 
     window.addEventListener('resize', setTransform);
     window.addEventListener('scroll', onScroll);
-    /*
-    function showNextSlider() {
-      if (sliderChevs[1]) {
-       // sliderChevs[1].click();
-      }
-    }
-    setInterval(showNextSlider, 2000);
-    */
 
     const NAMES = {
         navigationItemClassName: 'navigation__item',
@@ -23,16 +15,16 @@
         phonesImages = document.querySelectorAll('.phone__image'),
         categories = document.querySelectorAll('.category'),
         portfolioImages = document.querySelectorAll('.portfolio__image'),
+        portfolio = portfolioImages[0].parentNode,
         slides = document.querySelector('.slider-wrapper'),
         sliderChevs = document.querySelectorAll('.slider-chev'),
         formPopupOverlay = document.querySelector('.get-quote__form .overlay');
 
     var slidesCount = document.querySelectorAll('.slide').length,
-        portfolioImagesOrders = Array.from({ length: portfolioImages.length }, (v, k) => k),
         section = document.querySelectorAll("section"),
         sections = {},
         pos = 0,
-        i = 0;
+        sectionKey = 0;
     sections[header.id] = header.offsetTop - header.offsetHeight;
 
     Array.prototype.forEach.call(section, function(e) {
@@ -54,8 +46,8 @@
         chev.addEventListener("click", nextSlider);
     });
 
-    function currentSlide(i) {
-        return (i + slidesCount) % slidesCount;
+    function currentSlide(index) {
+        return (index + slidesCount) % slidesCount;
     }
 
     function nextSlider(e) {
@@ -112,9 +104,10 @@
     function anchorLinkHandler(e) {
         e.preventDefault();
 
-        if (!this.parentNode.classList.contains(NAMES.navigationItemClassName + '_selected')) {
+        let navItemClassList = this.parentNode.classList;
+        if (!navItemClassList.contains(NAMES.navigationItemClassName + '_selected')) {
             linksToAnchors.forEach(element => element.parentNode.classList.remove(NAMES.navigationItemClassName + '_selected'));
-            this.parentNode.classList.toggle(NAMES.navigationItemClassName + '_selected');
+            navItemClassList.toggle(NAMES.navigationItemClassName + '_selected');
         }
         let targetID = this.getAttribute('href');
         let target = document.querySelector(targetID);
@@ -145,38 +138,14 @@
     function categoryClick(e) {
         e.preventDefault();
 
-        let category = e.target.textContent;
-
         if (!this.classList.contains(NAMES.categoryClassName + '_selected')) {
 
             categories.forEach(element => element.classList.remove(NAMES.categoryClassName + '_selected'));
             this.classList.toggle(NAMES.categoryClassName + '_selected');
-
-            portfolioImages.forEach(function(image) {
-                image.style.display = 'none';
-            });
-
-            animate(portfolioImages);
+            for (let i = portfolioImages.length; i >= 0; i--) {
+               portfolio.appendChild(portfolioImages[Math.random() * i | 0]);
+            }
         }
-    }
-
-    function animate(item) {
-        portfolioImagesOrders.sort(function() {
-            return 0.5 - Math.random();
-        });
-
-        (function show(counter) {
-            setTimeout(function() {
-                if (item[counter]) {
-                    item[counter].style.display = null;
-                    item[counter].style.order = portfolioImagesOrders[counter];
-                    counter++;
-                    if (counter < item.length) {
-                        show(counter);
-                    }
-                }
-            }, 30);
-        })(0);
     }
 
     function onScroll() {
@@ -191,10 +160,10 @@
             linksToAnchors.forEach(element => element.parentNode.classList.remove(NAMES.navigationItemClassName + '_selected'));
             linksToAnchors[linksToAnchors.length - 1].parentNode.classList.add(NAMES.navigationItemClassName + '_selected');
         } else {
-            for (i in sections) {
-                if (sections[i] <= scrollPosition) {
+            for (sectionKey in sections) {
+                if (sections[sectionKey] <= scrollPosition) {
                     linksToAnchors.forEach(element => {
-                        if (element.getAttribute('href') == '#' + i) {
+                        if (element.getAttribute('href') == '#' + sectionKey) {
                             element.parentNode.classList.add(NAMES.navigationItemClassName + '_selected');
                         } else {
                             element.parentNode.classList.remove(NAMES.navigationItemClassName + '_selected');
